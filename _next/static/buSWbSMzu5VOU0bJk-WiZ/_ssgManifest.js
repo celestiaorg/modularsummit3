d24ca71db8801b802b3cb71b8deb92a9e6058e64
YazzyYaz <YazzyYaz@users.noreply.github.com>
2024-07-09 06:55:26 +0000

    deploy: e6c3c16530069a0a558a1a2cda2e75a004e4b994

---
+++ _next/static/buSWbSMzu5VOU0bJk-WiZ/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fagenda\u002F[day]","\u002Fagenda\u002F[day]\u002F[stage]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/chunks/app/agenda/[day]/[stage]/page-bcfe7a9085fc0960.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[77],{},function(n){n.O(0,[358,288,582,661,904,925,971,23,744],function(){return n(n.s=4925)}),_N_E=n.O()}]);
